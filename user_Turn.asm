621a8b12d261dee1900a77eea718a389f4ba6074
emc200002 <129010394+emc200002@users.noreply.github.com>
2023-04-02 00:22:05 -0500

    added isValid(), checks if line '-' or '|' already exists
    - index within 'array' = (baseAddr) + actualColumn + (actualRow * rowLength)
    - accesses address of 'array' from register $s0 (see grid.asm)
    - if user enters a coordinate that already has a line, prints invalid-input message and loops back for input

--- user_Turn.asm
+++ user_Turn.asm
@@ -1,28 +1,13 @@
-#################### Elizabeth Cadungog, EMC200002
-# 8 COLUMNS: 1 - 17
-# 	Horizontal lines: even	
-# 		- valid: 2, 4, 6, 8, 10, 12, 14, 16
-# 	Vertical lines: odd
-# 		- valid: 1, 3, 5, 7, 9, 11, 13, 15, 17
-
-# 6 ROWS: 1 - 13
-# 	Horizontal lines: odd
-# 		- valid: 1, 3, 5, 7, 9, 11, 13
-# 	Vertical lines: even
-# 		- valid: 2, 4, 6, 8, 10, 12
-
-# note: User views first row/col as index ‘1’. In Assembly, it is actually index ‘0’.
-
-####################
+# Elizabeth Cadungog, EMC200002
 
 ## RETURNS TWO VALUES ##
 #	$v0 = row index
 #	$v1 = col index
 
 .data
-	turnStart: .asciiz "USER TURN"
-	rowPrompt: .asciiz "\n\tEnter a valid row number (1 - 13): "	
-	colPrompt: .asciiz "\tEnter a valid column number (1 - 17): "
+	turnStart: .asciiz "\nUSER TURN"
+	rowPrompt: .asciiz "\nEnter a valid row number (1 - 13): "	
+	colPrompt: .asciiz "Enter a valid column number (1 - 17): "
 	
 	evenCol: .asciiz "\tPlease enter an even # column.\n"
 	oddCol: .asciiz "\tPlease enter an odd # column.\n"
@@ -34,6 +19,8 @@
 	rowNum: .word 0		# ranges 1 - 13 for user	# index 0 - 12
 	colNum: .word 0		# ranges 1 - 17 for user	# index 0 - 16
 
+	rowLength: .word 17	# each row has a width of 17
+	invalidStr: .asciiz "The given-coordinate already has a line. Please enter another value.\n"
 .text
 
 .globl user_Turn
@@ -97,7 +84,7 @@ user_Turn:
 		
 	oddRow: # rowNum is odd, colNum must be even
 		andi	$t2, $t0, 1		# if colNum is even, $t2 = 0	# else, colNum is odd, $t2 = 1
-		beq	$t2, $zero, turn_End	# $t2 = 0, colNum is even. Jump to turn_End
+		beq	$t2, $zero, isValid
 		
 		# else, colNum is odd. Prompt for an even column and loop
 		li      $v0, 4
@@ -108,7 +95,7 @@ user_Turn:
 		
 	evenRow: # rowNum is even, colNum must be odd			
 		andi	$t2, $t0, 1		# if colNum is even, $t2 = 0	# else colNum is odd, $t2 = 1
-		bne	$t2, $zero, turn_End	# $t2 = 1, colNum is odd. Jump to turn_End
+		bne	$t2, $zero, isValid
 		
 		# else, colNum is even. Prompt for an odd column and loop
 		li      $v0, 4
@@ -116,7 +103,52 @@ user_Turn:
 		move    $a0, $t5
 		syscall
 		j getCol
-		
+	
+isValid: # confirm that user-coordinate is valid, i.e. NOT a '+', '-', or '|'		
+	move 	$t1, $s0		# $t1 = base-address of array
+	lb	$t0, ($t1)		# get first byte of array
+
+	# check for end of string
+	beq 	$t0, $zero, getRow	# if end of string, prompt for input again
+	
+	# calculate index of user-coordinate
+	# Index = (baseAddr) + actualColumn + (actualRow * rowLength)	
+	lw	$t2, rowNum	# loads rowNum into $t2
+	addi	$t2, $t2, -1		# gets actual rowNum
+	lw	$t3, colNum	# loads colNum into $t3
+	addi	$t3, $t3, -1		# gets actual colNum
+	lw	$t4, rowLength	# loads rowLength into $t4
+	mul	$t2, $t2, $t4	# $t2 = actualRow * rowLength
+	add	$t5, $t3, $t2	# $t5 = offset = actualColumn + (actualRow * rowLength)
+	
+	# add offset to base address of array
+	move 	$t0, $s0	# $t0 = address of array
+	lb	$t1, ($t0)	# $t1 = first byte from array, index 0
+	beq	$t1, $zero, getRow	# check if end of string, fixme (safety net, possibly remove)
+	add	$t2, $t0, $t5	# index = baseAddress + offset
+	lb	$t3, ($t2)	# load byte at given coordinate
+	
+	# $t3 = index within array for the user-given coordinate
+	# check that the index is not already occupied with a '-' (odd row) or a '|' (even row)
+ 	lw 	$t1, rowNum
+	andi 	$t2, $t1, 1 		# if rowNum is even, $t2 = 0	# else rowNum is odd, $t2 = 1
+	
+	# IF EVEN ROW, jump to check. ELSE ODD ROW, set $t1 = '-' before check.
+	li 	$t0, '|'	# char to compare to for even rows
+	beq	$t2, $zero, checkChar	# $t2 = 0, rowNum is even. Check for '|'.
+	li 	$t0, '-'		# Else rowNum is odd, check for '-'
+	
+checkChar:		 	 	 	 	 	 	
+	# check if char is a line
+	bne 	$t3, $t0, turn_End	# VALID: jump to exit if the char is not already a line
+					# else, INVALID: print error message and loop for input
+	# print invalid input string
+	li      $v0, 4
+	la	$t5, invalidStr		
+	move    $a0, $t5
+	syscall
+	j 	getRow
+					
 turn_End:
 	# display values entered by user
 		# string 1/3
@@ -150,7 +182,7 @@ turn_End:
  		# subtract 1 from both values
  		addi	$t0, $t0, -1
  		addi	$t1, $t1, -1
- 		# $v0 = rowNum - 1, $v1 = colNUm - 1								
+ 		# $v0 = rowNum - 1, $v1 = colNum - 1								
  		add	$v0, $t0, $zero
  		add	$v1, $t1, $zero
  																		
@@ -160,3 +192,21 @@ turn_End:
 	
 	# return
 	jr      $ra
+
+
+####################
+# 8 COLUMNS: 1 - 17
+# 	Horizontal lines: even	
+# 		- valid: 2, 4, 6, 8, 10, 12, 14, 16
+# 	Vertical lines: odd
+# 		- valid: 1, 3, 5, 7, 9, 11, 13, 15, 17
+
+# 6 ROWS: 1 - 13
+# 	Horizontal lines: odd
+# 		- valid: 1, 3, 5, 7, 9, 11, 13
+# 	Vertical lines: even
+# 		- valid: 2, 4, 6, 8, 10, 12
+
+# note: User views first row/col as index ‘1’. In Assembly, it is actually index ‘0’.
+
+####################
