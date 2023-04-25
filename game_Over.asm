ca5b0d3da1db8cad85c8ce28d29d2b368687ff56
AstroResonance <104485004+AstroResonance@users.noreply.github.com>
2023-04-25 17:03:10 -0500

    Update game_Over.asm
    This version works

--- game_Over.asm
+++ game_Over.asm
@@ -5,6 +5,9 @@
 user_Wins: .asciiz "You win!"
 computer_Wins: .asciiz  "Computer wins,"
 tie_Msg: .asciiz  "It's a tie."
+checking_End: .asciiz "Checking the end" 
+InGO: .asciiz "In Game Over"
+ExitMsg: .asciiz "Exiting"
 .globl game_Over
 .text
 
@@ -13,12 +16,13 @@ tie_Msg: .asciiz  "It's a tie."
 move $t0, $s0
 
 game_Over:
+    
     #Counter for C's
     li $t1, 0           
     #Counter for U's
-    li $t2, 0           
-    #Counter for white spaces
-    li $t3, 0           
+    li $t2, 0     
+    
+    j loop           
 
 loop:
     # Load a byte from array into $t4
@@ -30,11 +34,10 @@ loop:
 
     li $t5, 'C'
     li $t6, 'U'
-    li $t7, ' '
 
     beq $t4, $t5, increment_C
-    beq $t4, $t6, increment_U
-    beq $t4, $t7, increment_space
+    beq $t4, $t6, increment_U 
+    
     j loop
 
 increment_C:
@@ -47,17 +50,19 @@ increment_U:
     addi $t2, $t2, 1    
     j loop
 
-increment_space:
-    #Increment white space counter
-    addi $t3, $t3, 1    
-    j loop
+
 
 check_end:
-    bgt $t3, 0, user_Turn
-
-    blt $t1, $t2, U_wins
-    bgt $t1, $t2, C_wins
-    beq $t1, $t2, tie
+    
+    
+    #add increments together
+    add $s2, $t2, $t1    
+    add $s3, $zero, $t1
+    add $s4, $zero, $t2
+    blt $s2, 48, return
+    blt $s3, $s4, U_wins
+    bgt $s3, $s4, C_wins
+    beq $s3, $s4, tie
 
 U_wins:
     li $v0, 4
@@ -73,9 +78,13 @@ C_wins:
 
 tie:
     li $v0, 4
-    la $a0, tie_Msg 
+    la $a0, tie_Msg
     syscall
+    
     j exit
+    
+return:
+jr $ra
 exit:
-    li $v0, 10   
-    syscall
+    li $v0, 10
+   syscall
